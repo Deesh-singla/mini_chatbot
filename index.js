611c1b249c65f6549d93ec53e20d83f5c14f1a9b
Deesh Singla <deeshsingla@gmail.com>
2026-04-28 10:35:10 +0530

    added chat history logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { ChatMistralAI } from "@langchain/mistralai";
 import { configDotenv } from "dotenv";
 import readline from "readline";
+import { AIMessage, HumanMessage, SystemMessage } from "@langchain/core/messages";
 
 configDotenv();
 
@@ -14,15 +15,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const chatHistory = [
+    new SystemMessage("you are a helpur ai assistent")
+]
+
 function askQuestion() {
     rl.question("User: ", async (input) => {
         if (input.toLowerCase() === "exit") {
             rl.close();
+            console.log(chatHistory);
             return;
         }
 
         try {
+            chatHistory.push(new HumanMessage(input));
             const res = await model.invoke(input);
+            chatHistory.push(new AIMessage(res.content));
             console.log("AI:", res.content);
         } catch (err) {
             console.error("Error:", err.message);
